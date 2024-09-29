e3763901876a57930dfe2b09534dd36cae1f67a5
maximiliensorret <ge58fuh@mytum.de>
2024-09-29 16:05:52 +0200

    second commit

--- task-1/ucgs_water3.py
+++ task-1/ucgs_water3.py
@@ -5,9 +5,9 @@ import ucs
 goal_node, n_visits = ucs.uniform_cost_graph_search(problem)
 if goal_node is not None:
     print("Solution")
-    print("========")
+    print("======")
     utils.print_solution(goal_node)
-    print("========")
+    print("======")
     print("Path cost = %d" % goal_node[3])
     print("Number of Visited States = %d" % n_visits)
 else:
